24eacac093c4e210f30d4bf6ed0ac7f7c37969e7
aligredo <aliahmedismail37@gmail.com>
2018-11-29 17:05:48 +0200

    getPetsByGender implemented

--- backend/api/controllers/pet.controller.js
+++ backend/api/controllers/pet.controller.js
@@ -31,14 +31,14 @@ module.exports.getAllPets = function(req, res, next){
 };
 
 module.exports.getPetsByOwnerUsername = function(req, res, next) {
-	if (!Validations.isString(req.body.username)) {
+	if (!Validations.isString(req.params.username)) {
 		return res.status(422).json({
 			err: null,
 			msg: "username parameter must be a valid String.",
 			data: null
 		});
 	}
-	Pet.find({ ownerUsername: req.body.username }).exec(function(err, pets) {
+	Pet.find({ ownerUsername: req.params.username }).exec(function(err, pets) {
 		if (err) {
 			return next(err);
 		}
@@ -51,14 +51,14 @@ module.exports.getPetsByOwnerUsername = function(req, res, next) {
 };
 
 module.exports.getPetsByType = function(req, res, next) {
-	if (!Validations.isString(req.body.type)) {
+	if (!Validations.isString(req.params.type)) {
 		return res.status(422).json({
 			err: null,
 			msg: "type must be a valid String.",
 			data: null
 		});
 	}
-	Pet.find({ type: req.body.type }).exec(function(err, pets) {
+	Pet.find({ type: req.params.type }).exec(function(err, pets) {
 		if (err) {
 			return next(err);
 		}
@@ -71,20 +71,40 @@ module.exports.getPetsByType = function(req, res, next) {
 };
 
 module.exports.getPetsBySpecies = function(req, res, next) {
-	if (!Validations.isString(req.body.species)) {
+	if (!Validations.isString(req.params.species)) {
 		return res.status(422).json({
 			err: null,
 			msg: "species must be a valid String.",
 			data: null
 		});
 	}
-	Pet.find({ species: req.body.species }).exec(function(err, pets) {
+	Pet.find({ species: req.params.species }).exec(function(err, pets) {
 		if (err) {
 			return next(err);
 		}
 		res.status(200).json({
 			err: null,
 			msg: "Pets are retrieved successfully.",
+			data: pets
+		});
+	});
+};
+
+module.exports.getPetsByGender = function(req, res, next) {
+	if (!Validations.isString(req.params.gender)) {
+		return res.status(422).json({
+			err: null,
+			msg: "gender must be a valid String.",
+			data: null
+		});
+	}
+	Pet.find({ gender: req.params.gender }).exec(function(err, pets) {
+		if (err) {
+			return next(err);
+		}
+		res.status(200).json({
+			err: null,
+			msg: "gender are retrieved successfully.",
 			data: pets
 		});
 	});
